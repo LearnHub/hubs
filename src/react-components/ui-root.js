bd25f418985d594caef691a266bc745708691a97
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2021-04-20 16:07:22 +0100

    Cherry pick of restored people count PR

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -1350,6 +1350,7 @@ class UIRoot extends Component {
                         )}
                         <PeopleMenuButton
                           active={this.state.sidebarId === "people"}
+                          presences={this.props.presences}
                           onClick={() => this.toggleSidebar("people")}
                         />
                       </ContentMenu>
