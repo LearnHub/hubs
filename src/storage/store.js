b716bf50e65ddcc20fdb4b6806d4f11b41802af4
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2021-11-08 08:58:42 +0000

    Use "high" quality on mobile platforms

--- src/storage/store.js
+++ src/storage/store.js
@@ -23,7 +23,8 @@ export const defaultMaterialQualitySetting = (function() {
     if (qsMobileDefault && MATERIAL_QUALITY_OPTIONS.indexOf(qsMobileDefault) !== -1) {
       return qsMobileDefault;
     }
-    return "low";
+    // AVN: All AVW scenes are optimized to run at high quality
+    return "high";
   }
 
   const qsDefault = qsGet("default_material_quality");
