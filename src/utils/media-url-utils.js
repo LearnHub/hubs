80e9ad58751cc642bb0eb8245f586437b029511c
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2022-02-09 15:25:43 +0000

    Local dev hack

--- src/utils/media-url-utils.js
+++ src/utils/media-url-utils.js
@@ -12,7 +12,8 @@ nonCorsProxyDomains.push(document.location.hostname);
 // AVN: data domain doesn't require CORS
 nonCorsProxyDomains.push("data.avncloud.com");
 // AVN: running as localhost fails to fetch objects.gltf because it tries to invoke the proxy
-nonCorsProxyDomains.push("me.avncloud.com");
+nonCorsProxyDomains.push("me.eduverse.com");
+nonCorsProxyDomains.push("me-eduverse-assets.me-hub.link");
 
 const commonKnownContentTypes = {
   gltf: "model/gltf",
