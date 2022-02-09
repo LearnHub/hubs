3a218bcf3d15d7e9d17b2d3bcd4506a3dc7a81d5
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2022-02-09 15:23:08 +0000

    Reinstate clickable web links for external sites

--- src/components/media-loader.js
+++ src/components/media-loader.js
@@ -351,6 +351,16 @@ AFRAME.registerComponent("media-loader", {
     }
 
     try {
+
+      // Short circuit for external web links (don't bother with fetching content types and thumbnails)
+      if(!avnBridge.isAvnUrl(src)) {
+        // Change image to be a 1x1 transparent PNG (image mesh provides the hover target)
+        this.el.setAttribute("media-image", { src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=", contentType: "image/png" });
+        this.el.setAttribute("hover-menu__link", { template: "#link-hover-menu", isFlat: true });
+        this.el.setAttribute("shape-helper", { type: SHAPE.BOX, minHalfExtent: 0.04 });  
+        return;
+      }
+
       if ((forceLocalRefresh || srcChanged) && !this.showLoaderTimeout) {
         this.showLoaderTimeout = setTimeout(this.showLoader, 100);
       }
