01150a68ce944259c45628fb623d8057721ca419
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2022-05-12 17:10:59 +0100

    Reinstated the webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const selfsigned = require("selfsigned");
 const webpack = require("webpack");
 const cors = require("cors");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HTMLWebpackInjectAttributesPlugin = require("html-webpack-inject-attributes-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
@@ -253,8 +254,11 @@ module.exports = async (env, argv) => {
   };
 
   const devServerHeaders = {
-    "Access-Control-Allow-Origin": "*"
-  };
+    "Access-Control-Allow-Origin": "*",
+    "Cross-Origin-Opener-Policy": "same-origin",
+    "Cross-Origin-Embedder-Policy": "require-corp",
+    "Cross-Origin-Resource-Policy": "cross-origin"
+};
 
   // Behind and environment var for now pending further testing
   if (process.env.DEV_CSP_SOURCE) {
@@ -288,7 +292,9 @@ module.exports = async (env, argv) => {
     },
     output: {
       filename: "assets/js/[name]-[chunkhash].js",
-      publicPath: process.env.BASE_ASSETS_PATH || ""
+      publicPath: process.env.BASE_ASSETS_PATH || "",
+      // May be required for dynamically loaded scripts
+      crossOriginLoading: "anonymous"
     },
     devtool: argv.mode === "production" ? "source-map" : "inline-source-map",
     devServer: {
@@ -660,7 +666,12 @@ module.exports = async (env, argv) => {
       // Extract required css and add a content hash.
       new MiniCssExtractPlugin({
         filename: "assets/stylesheets/[name]-[contenthash].css",
-        disable: argv.mode !== "production"
+        disable: argv.mode !== "production",
+        attributes: {
+          // May be required for dynamically loaded stylesheets
+          "crossorigin": "anonymous"
+        },
+  
       }),
       // Define process.env variables in the browser context.
       new webpack.DefinePlugin({
@@ -680,7 +691,12 @@ module.exports = async (env, argv) => {
           BASE_ASSETS_PATH: process.env.BASE_ASSETS_PATH,
           APP_CONFIG: appConfig
         })
+      }),
+      // Add crossorigin attribute to static <script> and <style> tags
+      new HTMLWebpackInjectAttributesPlugin({
+        crossorigin: "anonymous"
       })
+
     ]
   };
 };
