8261132df74dd19e9b6019d0bc089e5c60a92c7a
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2022-06-24 11:29:43 +0100

    Skip preload screen when in an iframe

--- src/hub.js
+++ src/hub.js
@@ -220,11 +220,15 @@ const NOISY_OCCUPANT_COUNT = 30; // Above this # of occupants, we stop posting j
 const qs = new URLSearchParams(location.search);
 const isMobile = AFRAME.utils.device.isMobile();
 const isMobileVR = AFRAME.utils.device.isMobileVR();
+// AVN: Don't bother with preload screen and the extra logic relating to embedding
+const isEmbed = false;
+/*
 const isEmbed = window.self !== window.top;
 if (isEmbed && !qs.get("embed_token")) {
   // Should be covered by X-Frame-Options, but just in case.
   throw new Error("no embed token");
 }
+*/
 
 THREE.Object3D.DefaultMatrixAutoUpdate = false;
 
