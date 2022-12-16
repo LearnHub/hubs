94b71ae9224d25b6e66ef5183473f38a231136a1
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2022-12-16 10:38:39 +0000

    Improved layout for home page

--- src/react-components/home/AVNHomePage.js
+++ src/react-components/home/AVNHomePage.js
@@ -1,120 +1,53 @@
-import { HealthCheckResponse_ServingStatus } from "connect-sdk/dist/gen/grpc/health/v1/healthcheck_pb";
-import React, { useContext, useEffect } from "react";
-import { PageContainer } from "../layout/PageContainer";
+import React from "react";
 import { AVN } from "../../avn-bridge"
-import { sleep } from "../../utils/async-utils";
-import { AppLogo } from "../misc/AppLogo";
-import styles from "./HomePage.scss";
 import { isLocalClient } from "../../utils/phoenix-utils";
 import { store } from "../../utils/store-instance";
+import { LoadingScreen } from "../room/LoadingScreen";
 
 export class AVNHomePage extends React.Component {
 
-  static propTypes = {
-    // onPreloadLoadClicked: PropTypes.func,
-    // embed: PropTypes.bool,
-    // onLoaded: PropTypes.func,
-    // activeObject: PropTypes.object,
-    // selectedObject: PropTypes.object,
-    // breakpoint: PropTypes.string,
-    // canVoiceChat: PropTypes.bool
-  };
-
   state = {
-    statusMessage: "Starting...",
-    // enterInVR: false,
-    // autoExitTimerStartedAt: null,
+    message: "Loading...",
+    errorMessage: null,
   };
 
   constructor(props) {
     super(props);
   }
 
-  componentDidUpdate(prevProps) {
-    console.log("AVNHomePage::componentDidUpdate", this.props, prevProps)
-    // const { hubChannel, showSignInDialog } = this.props;
-    // if (hubChannel) {
-    //   const { signedIn } = hubChannel;
-    //   if (signedIn !== this.state.signedIn) {
-    //     this.setState({ signedIn });
-    //   }
-    // }
-  }
-
   async componentDidMount() {
-    console.log("AVNHomePage::componentDidMount")
-
-    // Belt and braces code to create a new dimension
-
-    const accessToken = store.state.credentials?.extras?.access_token;
-    if(accessToken) {
-      console.log("AVN authenticating with existing token")
-      this.setState({statusMessage: "Authenticating..." })
-      await AVN.authenticate(accessToken)
-    } else {
-      console.log("AVN no token found so connection will be anonymous")
-    }
-    this.setState({statusMessage: "Checking AVN Cloud connection..." })
-    if(await AVN.isHealthy()) {
-      this.setState({statusMessage: "Opening new dimension..." })
-      if(await AVN.openNewDimension()) {
-        this.setState({statusMessage: `New dimension is open ${AVN.dimensionId} with default asset ID ${AVN.assetId}` })  
+    try {
+      const accessToken = store.state.credentials?.extras?.access_token;
+      if (accessToken) {
+        console.log("AVN: authenticating with existing token")
+        this.setState({ message: "Authenticating..." })
+        await AVN.authenticate(accessToken)
       } else {
-        this.setState({statusMessage: "Failed to create a new dimension" })
+        console.log("AVN: no token found so connection will be anonymous")
       }
-      
-      const room = await AVN.Connect.Rooms.findRoom({dimensionId: AVN.dimensionId, assetId: AVN.assetId})
-      this.setState({statusMessage: `Found room ${room.domain} ${room.roomId}` })
-            
-      if (isLocalClient()) {
-        document.location.replace(`/hub.html?hub_id=${room.roomId}`)
+      this.setState({ message: "Checking Eduverse connection..." })
+      if (await AVN.isHealthy()) {
+        this.setState({ message: "Opening new dimension..." })
+        if (await AVN.openNewDimension()) {
+          console.log(`New dimension is open ${AVN.dimensionId} with default asset ID ${AVN.assetId}`)
+          this.setState({ message: "Finding a room..." })
+          const room = await AVN.Connect.Rooms.findRoom({ dimensionId: AVN.dimensionId, assetId: AVN.assetId })
+          console.log(`Found room ${room.domain} ${room.roomId}`)
+          const roomUrl = isLocalClient() ? `/hub.html?hub_id=${room.roomId}` : `https://${room.domain}/${room.roomId}`
+          this.setState({ message: `Joining room...` })
+          document.location.replace(roomUrl)
+        } else {
+          this.setState({ message: "An unexpected error occured", errorMessage: "Failed to create a new dimension" })
+        }
       } else {
-        document.location.replace(`https://${room.domain}/${room.roomId}`)
+        this.setState({ message: "An unexpected error occured", errorMessage: "Failed to connect to AVN Cloud" })
       }
-    
-
-    } else {
-      this.setState({statusMessage: "Failed to connect to AVN Cloud" })
+    } catch(error) {
+      this.setState({ message: "An unexpected exception occured", errorMessage: error?.message })
     }
   }
-
-  componentWillUnmount() {
-    // this.props.scene.removeEventListener("loaded", this.onSceneLoaded);
-    // this.props.scene.removeEventListener("exit", this.exitEventHandler);
-    // this.props.scene.removeEventListener("share_video_enabled", this.onShareVideoEnabled);
-    // this.props.scene.removeEventListener("share_video_disabled", this.onShareVideoDisabled);
-    // this.props.scene.removeEventListener("share_video_failed", this.onShareVideoFailed);
-    // this.props.scene.removeEventListener("action_media_tweet", this.onTweet);
-    // this.props.store.removeEventListener("statechanged", this.storeUpdated);
-    // window.removeEventListener("concurrentload", this.onConcurrentLoad);
-    // window.removeEventListener("idle_detected", this.onIdleDetected);
-    // window.removeEventListener("activity_detected", this.onActivityDetected);
-    // window.removeEventListener("focus_chat", this.onFocusChat);
-  }
-
-  onSceneLoaded = () => {
-    // console.log("UI root scene has loaded");
-    // this.setState({ sceneLoaded: true });
-  };
-
-  onSubscribeChanged = async () => {
-    // if (!this.props.subscriptions) return;
-
-    // await this.props.subscriptions.toggle();
-    // this.updateSubscribedState();
-  };
 
   render() {
-//    const hide = this.state.hide || this.props.hide;
-    if (true /*this.state.isRecordingMode*/) {
-      return (
-        <div>
-          <div>
-            <AppLogo />
-          </div>
-          <p>{this.state.statusMessage}</p>
-        </div>
-      );
-    }
+    return <LoadingScreen message={this.state.message} errorMessage={this.state.errorMessage} />;
   }
 }
