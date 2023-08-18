9d5a71c9be45522879181a623fef5cfb42aab586
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2023-08-18 11:35:33 +0100

    Replace hack for now

--- src/components/gltf-model-plus.js
+++ src/components/gltf-model-plus.js
@@ -387,7 +387,11 @@ function runMigration(version, json) {
 }
 
 const convertStandardMaterialsIfNeeded = (object, gltf) => {
-  const materialQuality = window.APP.store.state.preferences.materialQualitySetting
+  // COMPRESSED_SCENE_UPDATE replace this assignment with the one commented out below
+  const materialQuality = gltf?.asset?.generator && gltf.asset.generator.startsWith("Khronos glTF Blender")
+    ? "low"
+    : window.APP.store.state.preferences.materialQualitySetting
+  //const materialQuality = window.APP.store.state.preferences.materialQualitySetting
   updateMaterials(object, material => convertStandardMaterial(material, materialQuality));
   return object;
 };
