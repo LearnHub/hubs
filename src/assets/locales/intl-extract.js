08664f701da94b9f85f60501b1a338582823d497
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2023-11-07 13:37:55 +0000

    Script logic for finding missing translations

--- src/assets/locales/intl-extract.js
+++ src/assets/locales/intl-extract.js
@@ -11,9 +11,9 @@ async function main(argv) {
   const basePath = path.resolve(__dirname, "..", "..", "..");
   const files = await asyncGlob("../../../src/**/*.js", { cwd: __dirname, absolute: true });
 
-  if (command === "en") {
-    const outPath = path.join(__dirname, "en.json");
-
+  // Has a specific locale been requested?
+  if (command) {
+    console.log(`Extracting all messages...`)
     const extractedMessages = await extract(files, {
       extractSourceLocation: false,
       extractFromFormatMessageCall: true,
@@ -27,13 +27,37 @@ async function main(argv) {
         }
       }
     });
+    const enMessages = JSON.parse(extractedMessages);
+    console.log(`Found ${Object.keys(enMessages).length} 'en' messages`)
 
-    await asyncWriteFile(outPath, extractedMessages);
-
-    console.log(`Wrote file to ${outPath}`);
+    const localePath = path.join(__dirname, `${command}.json`); 
+    if (command === "en") {
+      // "en" is the default locale, so just write out the extracted messages
+      await asyncWriteFile(localePath, extractedMessages);  
+      console.log(`Wrote file to ${outPath}`);
+    } else {      
+      // Load the locale and identify missing translations
+      console.log(`Reading locale messages for '${command}'...`)
+      const localeFile = await fs.promises.readFile(localePath);
+      const localeMessages = JSON.parse(localeFile);
+      console.log(`Read ${Object.keys(localeMessages).length} '${command}' messages`)
+      // Remove messages already translated
+      for(let localeMessage in localeMessages) {        
+        delete enMessages[localeMessage]
+      }
+      const missingTranslationCount = Object.keys(enMessages).length
+      if(missingTranslationCount > 0) {
+        console.warn(`${missingTranslationCount} 'en' messages have no translation`)
+        const missingTranslationPath = path.join(__dirname, `${command}-missing.json`);
+        await asyncWriteFile(missingTranslationPath, JSON.stringify(enMessages));
+        console.log(`Missing translations written to ${missingTranslationPath}`)
+      } else {
+        console.info(`No missing translations`)
+      }
+    }
   } else {
+    // Write out detailed information about all the translations
     const outPath = path.join(__dirname, "extracted-messages.json");
-
     const extractedMessages = await extract(files, {
       extractSourceLocation: true,
       extractFromFormatMessageCall: true,
