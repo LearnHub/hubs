1f38fcc68847a8dd6e4b92a3acc713c0b2e21a45
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2023-12-19 11:03:55 +0000

    Default NAF schema did not include scale

--- src/network-schemas.js
+++ src/network-schemas.js
@@ -130,6 +130,25 @@ function registerNetworkSchemas() {
     ]
   });
 
+  // AVN: Moveable extension
+  NAF.schemas.add({
+    template: "#moveable-media",
+    components: [
+      {
+        component: "position",
+        requiresNetworkUpdate: vectorRequiresUpdate(0.001)
+      },
+      {
+        component: "rotation",
+        requiresNetworkUpdate: vectorRequiresUpdate(0.5)
+      },
+      {
+        component: "scale",
+        requiresNetworkUpdate: vectorRequiresUpdate(0.001)
+      },
+    ],
+  });
+
   NAF.schemas.add({
     template: "#interactable-emoji",
     components: [
