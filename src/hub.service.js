2b16f350181ac3eca15639dd72984032b4e69ada
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2025-06-30 10:59:24 +0100

    Service workers can't really key off URL parameters because they are cross request

--- src/hub.service.js
+++ src/hub.service.js
@@ -21,8 +21,7 @@ async function checkAltServers() {
         // Don't check more often than every 60 minutes
         nextCheckTimestamp = now + 60 * 60_000
         try {
-            const searchParams = new URLSearchParams(self.location.search)
-            const AltServerEndpoint = searchParams.get("restHost") === "alpha" 
+            const AltServerEndpoint = self.location.hostname === "gb.eduverse.com" 
                 ? "https://rest-alpha.avncloud.com/v1/avnfs/altservers"
                 : "https://rest.avncloud.com/v1/avnfs/altservers"        
             console.log(`AVNSW: Downloading altservers from '${AltServerEndpoint}'...`)
